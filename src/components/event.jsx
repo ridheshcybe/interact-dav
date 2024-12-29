a953c8963c77751fcc0b90cef4ae5393a8a0a084
ridheshcybe <80441770+ridheshcybe@users.noreply.github.com>
2024-12-29 23:52:59 +0545

    fixed vercel error

--- src/components/event.jsx
+++ src/components/event.jsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import dynamic from "next/dynamic";
 
-const Map = dynamic(() => import("./Map"), {
+const Map = dynamic(() => import("@/components/map"), {
   loading: () => <p>Loading map...</p>,
   ssr: false,
 });
